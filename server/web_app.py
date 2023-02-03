40629bd42bf39847e82561a5a9f1f0fdc6823847
Sebastian Felgueras <sebastian.felgueras@gmail.com>
2023-02-03 19:34:35 -0300

    primera version funcional del servidor

--- server/web_app.py
+++ server/web_app.py
@@ -3,6 +3,7 @@ import pickle
 from datetime import date, timedelta
 import requests
 import sys
+import numpy as np
  
 sys.path.insert(0, '/home/sebastian/Documentos/sebastian/experimentos/clima/')
 from modelo import ModeloSKL
@@ -22,20 +23,29 @@ def partir(linea:str):
     res.append(linea[44:].strip())
     return res
 
-def procesar_archivo(contenido: str):
+def procesar_archivo(contenido: str,hoy: date):
     data_aero = list(contenido.splitlines())[2:26]
     data_aero = list(map(partir,data_aero))
     if len(data_aero) != 24 or sum(map(lambda x: x[7] != 'AEROPARQUE AERO',data_aero)) != 0:
-        return "cambiaron el formato :("
+        return None
     data_aero.sort(key=lambda x: int(x[1]))
+    data_aero = [list(map(lambda x: float(x),x[:-1])) for x in data_aero]
+    data = []
+    for l in data_aero:
+        l[0] = float(hoy.day)
+        l.insert(0,float(hoy.month))
+        data.append(l)
+    print(data)
+    data = np.array(data).flatten().reshape(1,-1)
+    return data
 
 
 
 base = "https://ssl.smn.gob.ar/dpd/descarga_opendata.php?file=observaciones/"
-global modelo
+global model
 
 with open("modelo.mod","rb") as f:
-    modelo = pickle.load(f)
+    model: ModeloSKL = pickle.load(f)
 
 app = Flask(__name__)
 
@@ -45,14 +55,19 @@ def predecir():
     archivo = f"datohorario{hoy.year}{hoy.month:02d}{hoy.day:02d}.txt"
     r = requests.get(base + archivo)
     if r.status_code != requests.codes.ok:
-        return "No fue posible conectarse con el SMN"
+        return f"No fue posible conectarse con el SMN, error {r.status_code}"
     contenido = str(r.content,encoding="cp1252")
     if contenido.find("El archivo no existe.") != -1:
         hoy = hoy - timedelta(days=1)
         archivo = f"datohorario{hoy.year}{hoy.month:02d}{hoy.day:02d}.txt"
-        print(hoy)
         r = requests.get(base + archivo)
         contenido = str(r.content,encoding="cp1252")
-    procesar_archivo(contenido)
+        if contenido.find("El archivo no existe.") != -1:
+            return "No pudimos conseguir los datos actuales para hacer la predicción"
 
-    return f"Predicciones para el {hoy + timedelta(days=1)}<br/>{data_aero}"
+    data = procesar_archivo(contenido,hoy)
+    if data is None:
+        return f"Hubo un problema al parsear los datos del archivo descargado del SMN. Este fue el contenido: {contenido}"
+    print(len(data))
+    t = model.predecir(data)
+    return f"Predicciones para el {hoy + timedelta(days=1)}<br/>Temperatura mínima: {t[0][0]}<br/>Temperatura máxima: {t[0][1]}"
